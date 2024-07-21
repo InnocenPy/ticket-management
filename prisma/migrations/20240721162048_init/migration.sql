22cffb5928d9bf75587ce53ef344352278f9b89f
innocent-dev <dktseydou@gmail.com>
2024-07-21 16:28:17 +0000

    prisma migration of data in mysql

---
+++ prisma/migrations/20240721162048_init/migration.sql
@@ -0,0 +1,11 @@
+-- CreateTable
+CREATE TABLE `Ticket` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `title` VARCHAR(225) NOT NULL,
+    `description` TEXT NOT NULL,
+    `status` ENUM('OUVERT', 'EN_COURS', 'FERME') NOT NULL DEFAULT 'OUVERT',
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
